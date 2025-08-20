f7a43b6f6a961545c47fb3dc1d38f3dc34230105
jeanport <pierreanatc522@gmail.com>
2025-08-20 11:28:42 -0300

    Iniciando projeto

---
+++ src/main/java/com/jean/portacademia/PortacademiaApplication.java
@@ -0,0 +1,13 @@
+package com.jean.portacademia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortacademiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortacademiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jean/portacademia/PortacademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jean.portacademia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortacademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
